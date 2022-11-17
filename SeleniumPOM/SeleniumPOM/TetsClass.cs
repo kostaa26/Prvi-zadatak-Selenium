3df96b093bca2ba653155047d68c76bcde41f4d5
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-17 20:09:01 +0100

    Add new tests

--- SeleniumPOM/SeleniumPOM/TetsClass.cs
+++ SeleniumPOM/SeleniumPOM/TetsClass.cs
@@ -32,12 +32,21 @@ public class TestClass
     {
 
         Login("execute", "automation");
+        _homePage.Initial.SendKeys("KB");
+        _homePage.Title("Mr.");
+        _homePage.FirstName.SendKeys("Kosta");
+        _homePage.MiddleName.SendKeys("Blagojevic");
+        _homePage.Female.Click();
+        _homePage.Hindi.Click();
+        _homePage.SaveButton.Click();
+
     }
 
     [Test]
     public void TC02_OpenPopUp_ShouldDisplayed()
     {
         Login("execute", "automation");
+        _homePage.HtmlPopup.Click();
 
     
     }
@@ -47,6 +56,7 @@ public class TestClass
     public void TC03_OpenAlert_ShouldDisplayed()
     {
         Login("execute", "automation");
+        _homePage.Alert();
 
      
     }
